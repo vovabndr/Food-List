85da47fa1f8b74208f083727e8eb4274563750b4
vovabndr <vovabndr6661@icloud.com>
2018-06-07 23:43:21 +0300

    Initial Commit

---
+++ Food List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food List
+//
+//  Created by Владимир Бондарь on 6/7/18.
+//  Copyright © 2018 vbbv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
